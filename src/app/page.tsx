9c40e31f5f87f2478a4be45217f1b8ae57942d89
Jaqqueee <151760442+Jaqqueee@users.noreply.github.com>
2025-10-26 20:58:58 -0400

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
+import './globals.css';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="main font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20" style ={{position: "relative"}}>
+      <Image
+        className="dark:invert"
+        src="/umbccrop.jpg"
+        alt="Next.js logo"
+        style={{zIndex: -1}}
+        fill={true}
+        //width={100}
+        //height={100}
+        //priority
+      />
+        <div className="button">
+         {/* <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"> */}
+            <button>
+            BUTTONTEXT
+          </button>
+        </div>
+      {/*
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -21,10 +32,9 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            blah blah blah.
           </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -49,6 +59,19 @@ export default function Home() {
           >
             Read our docs
           </a>
+          {/* <Image
+          className="dark:invert"
+          src="/umbccrop.jpg"
+          alt="Next.js logo"
+          style={{zIndex: -1}}
+          fill={true}
+          //width={100}
+          //height={100}
+          //priority
+          /> 
+        <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+          TestingButton
+        </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -98,6 +121,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      */}
     </div>
   );
 }
