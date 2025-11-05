1017a5f00a843bb65d1b8e04714316f90966c42e
Jaqqueee <151760442+Jaqqueee@users.noreply.github.com>
2025-11-05 14:18:49 -0500

    changed formatting, now it actually fits full screen

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,25 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import style from '../Map/map.module.css';
+import * as L from "leaflet";
 
 function Map(){
     return(
-        <MapContainer className={style.map} center={[39.255632, -76.710665]} zoom={16} scrollWheelZoom={true}>
+        /*
+        className: Takes style from components\Map\map.module.css
+        center: center point of the map when the page loads
+        zoom: default zoom level when the page loads
+        scrollWheelzoom: allows the scroll wheel to perform zoom features
+        minZoom/maxZoom: the min and max zoom values that a user can use, these are to limit the user from zooming too far in or out
+        */
+        <MapContainer className={style.map} center={[39.255632, -76.710665]}
+             zoom={16} scrollWheelZoom={true} minZoom={16} maxZoom={18}>
             <TileLayer
                 attribution= '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            const svgElement
         </MapContainer>
     );
 }
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,6 +2,7 @@
 import dynamic from "next/dynamic";
 
 const Map = dynamic (()=> import('./Map'), {ssr: false})
+//Map.fitBounds([39.264927, -76.733514],[39.248579, -76.698081])
 
 export default function faqOnly(props){
     return <Map />
