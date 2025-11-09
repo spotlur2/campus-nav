e0a990b6fdcbba595f242aad66c941211674264c
jaqquee <jaqueroobs@gmail.com>
2025-11-09 00:09:12 -0500

    added all academic buildings

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,10 +6,26 @@ import * as L from "leaflet";
 
 const fillBlueOptions = { fillColor: 'blue' }
 
+//When the page loads up, the first val is y axis, more = move circle up, less = move circle down
+//second val is x axis, increase = move left, decrease = move right ***this val is negative already so watch signs
+
 const centerLibrary = [39.256510, -76.711616];
 const centerRAC = [39.252813, -76.712444];
 const centerAdmin = [39.253056, -76.713491];
 const centerITE = [39.253845, -76.714270];
+const centerEngie = [39.254540, -76.7139501];
+const centerFineArts = [39.255173, -76.7136548];
+const centerMeyerhoff = [39.254917, -76.712790];
+const centerUC = [39.254291, -76.713233];
+const centerSherman = [39.253636, -76.713423];
+const centerSondheim = [39.253441, -76.712769];
+const centerMath = [39.254107, -76.712454];
+const centerILSB = [39.253916, -76.710854];
+const centerCommons = [39.254916, -76.711014];
+const centerPhys = [39.254448, -76.709589];
+const centerPUP = [39.255166, -76.709107];
+const centerWellBeing = [39.256064, -76.708909];
+const centerPAHB = [39.25522, -76.715312];
 
 function Map(){
     return(
@@ -37,6 +53,48 @@ function Map(){
             </Circle>
             <Circle center={centerITE} pathOptions={fillBlueOptions} radius={35}>
                 <Popup>This is the ITE building</Popup>
+            </Circle>
+            <Circle center={centerEngie} pathOptions={fillBlueOptions} radius={35}>
+                <Popup>This is the Engineering Building</Popup>
+            </Circle>
+            <Circle center={centerFineArts} pathOptions={fillBlueOptions} radius={35}>
+                <Popup>This is the Fine Arts Building</Popup>
+            </Circle>
+            <Circle center={centerMeyerhoff} pathOptions={fillBlueOptions} radius={35}>
+                <Popup>This is the Meyerhoff Building</Popup>
+            </Circle>
+            <Circle center={centerUC} pathOptions={fillBlueOptions} radius={30}>
+                <Popup>This is the University Center</Popup>
+            </Circle>
+            <Circle center={centerSherman} pathOptions={fillBlueOptions} radius={37}>
+                <Popup>This is the Sherman Hall Building</Popup>
+            </Circle>
+            <Circle center={centerSondheim} pathOptions={fillBlueOptions} radius={25}>
+                <Popup>This is Sondheim Building</Popup>
+            </Circle>
+            <Circle center={centerMath} pathOptions={fillBlueOptions} radius={20}>
+                <Popup>This is the Math and Psychology Building</Popup>
+            </Circle>
+            <Circle center={centerCommons} pathOptions={fillBlueOptions} radius={40}>
+                <Popup>This is the Commons </Popup>
+            </Circle>
+            <Circle center={centerPhys} pathOptions={fillBlueOptions} radius={30}>
+                <Popup>This is the Physics Building</Popup>
+            </Circle>
+            <Circle center={centerPUP} pathOptions={fillBlueOptions} radius={30}>
+                <Popup>This is the Public Policy Building</Popup>
+            </Circle>
+            <Circle center={centerWellBeing} pathOptions={fillBlueOptions} radius={25}>
+                <Popup>This is Center for Well Being</Popup>
+            </Circle>
+            <Circle center={centerMath} pathOptions={fillBlueOptions} radius={20}>
+                <Popup>This is the Math and Psychology Building</Popup>
+            </Circle>
+            <Circle center={centerPAHB} pathOptions={fillBlueOptions} radius={50}>
+                <Popup>This is the Performing Arts and Humanities Building </Popup>
+            </Circle>
+            <Circle center={centerILSB} pathOptions={fillBlueOptions} radius={30}>
+                <Popup>This is the ILSB</Popup>
             </Circle>
         </MapContainer>
 
